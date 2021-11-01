e40631c0d57cee510ce5e830fa680bbb27d50a76
jmson2670 <jmson@narantech.com>
2021-11-01 22:32:41 +0900

    fix for api return form

--- main.py
+++ main.py
@@ -37,24 +37,35 @@ def get_test():
 #테스트 코드 // 개인용
 @app.get("/tt/{target_id}")
 def get_test(target_id):
-  print("target_id : ",target_id)
+  ret = {}
   real_lv = None
   _target_id = urllib.parse.quote_plus(target_id)
   url = urlopen('https://lostark.game.onstove.com/Profile/Character/'+_target_id)
   page_data = BeautifulSoup(url, "html.parser")
   attention = page_data.main.find("div", {"class": "profile-attention"})
   if attention:
-    print("hm?")
     return json.dumps(None)
   else:
-    print("maybe?")
     lv_tag = page_data.main.find("div", {"class": "level-info2__expedition"}).find_all("span")
     lv = lv_tag[-1]
     for con in lv:
       if isinstance(con, NavigableString):
         real_lv = con
     real_lv = str(real_lv).replace(",", "")
-    return int(real_lv)
+    char_list = page_data.main.find("div", {"id": "expand-character-list"}).find_all("button")
+    test_list = []
+    for _char_con in char_list:
+      char_url = _char_con.get('onclick')
+      if isinstance(char_url, str):
+        char_name = char_url.split('/')[-1]
+        char_name = char_name[:-1]
+        test_list.append(char_name)
+    #test block {{{
+
+    #}}}
+    ret['lv'] = real_lv
+    ret['char_list'] = test_list
+    return ret
 
 
 if __name__ == "__main__":
